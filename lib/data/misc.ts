0cb31b1705d21f010dfe3edc1ca9e0c87e8bfc81
idong-geon <idong-geon@example.com>
2026-07-09 11:03:24 +0900

    feat(reefer): update to Week 27 (Jul 3-9) - 7 vessels, removed 4 completed

--- lib/data/misc.ts
+++ lib/data/misc.ts
@@ -1,7 +1,7 @@
 import mangosteenKoreaExport from '../../data/mangosteen_kr_export.json';
 import octopusDomesticResource from '../../data/octopus_domestic_resource.json';
 import octopusGlobalCatch from '../../data/octopus_global_catch.json';
-import reeferWeek26 from '../../data/reefer_week26.json';
+import reeferWeek27 from '../../data/reefer_week27.json';
 import seasiaOemMaCandidates from '../../data/seasia_oem_ma_candidates.json';
 import seasiaOemVendors from '../../data/seasia_oem_vendors.json';
 import shrimpDashboard from '../../data/shrimp_dashboard.json';
@@ -10,7 +10,7 @@ const miscDatasets = {
   mangosteenKoreaExport,
   octopusDomesticResource,
   octopusGlobalCatch,
-  reeferWeek26,
+  reeferWeek27,
   seasiaOemMaCandidates,
   seasiaOemVendors,
   shrimpDashboard,
